ed3a959058dd1a750665196c303f3fce48c29ac7
MJ <muriloaj@gmail.com>
2018-11-03 20:26:26 -0500

    Enable swagger n remove https redirect

--- server/Startup.cs
+++ server/Startup.cs
@@ -25,23 +25,51 @@ namespace ats
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Creates TodoList in memory database
+            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+            //Force compatibility with Version 2.1.x
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            #region Swagger configuration
+
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "SNAPSHOT",
+                    Title = "Todo API",
+                    Description = "A simple example ASP.NET Core Web API to manage TodoItems",
+                    TermsOfService = "None",
+
+                });
+            });
+
+            #endregion
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseHsts();
-            }
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
 
-            app.UseHttpsRedirection();
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoItem management API ");
+                //set root to swagger.
+                c.RoutePrefix = string.Empty;
+            });
+
             app.UseMvc();
+
         }
     }
 }
