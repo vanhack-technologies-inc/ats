b1598fcc53fa9f1877ee253bc8d8ec82ed0087c2
MJ <muriloaj@gmail.com>
2018-11-03 20:16:53 -0600

    BUGFIX wrong import no startup

--- server/Startup.cs
+++ server/Startup.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using Swashbuckle.AspNetCore.Swagger;
-using TodoApi.Models;
+using ats.Models;
 
 namespace ats
 {
