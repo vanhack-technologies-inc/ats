578e2b9d4df6f3dd41677c63e1ce007d50f08eb5
MJ <muriloaj@gmail.com>
2018-11-06 16:47:05 -0600

    show up test status before run it off

--- server/bash-scripts/smoketest-basic.sh
+++ server/bash-scripts/smoketest-basic.sh
@@ -10,13 +10,50 @@ API=
 ###########
 ### Reusable behaviors
 ###########
+TESTS_PASSED_SUM=0
+TESTS_PASSED_LIST=
+TESTS_REPROVED_SUM=0
+TESTS_REPROVED_LIST=
+
 function rejected () {
-  echo "NOK. TEST $1 rejected.  Exiting..." && exit 1
+  TESTS_REPROVED_LIST="$TESTS_REPROVED_LIST:$1"
+  TESTS_REPROVED_SUM=$(echo $TESTS_REPROVED_LIST | grep ":" -c )
+  echo "NOK. TEST $1 rejected.  Exiting..." && echo_resume && exit 1
 }
 
+function rejected_bypassed () {
+  TESTS_REPROVED_LIST="$TESTS_REPROVED_LIST:$1"
+  TESTS_REPROVED_SUM=$(echo $TESTS_REPROVED_LIST | grep ":" -c )
+  echo "NOT PASSED:  $1 - This error was bypassed to move to next test"
+}
 
 function checked () {
+  TESTS_PASSED_LIST="$TESTS_PASSED_LIST:$1"
+  TESTS_PASSED_SUM=$(echo $TESTS_PASSED_LIST | grep ":" -c )
   echo "--- $1 Checked"
+}
+
+function echo_resume {
+  echo "########### Test Resume"
+  echo "### Tests approved: $TESTS_PASSED_SUM"
+  echo "### Tests reproved: $TESTS_REPROVED_SUM"
+  echo "### Total of tests executed: $(( $TESTS_REPROVED_SUM + $TESTS_PASSED_SUM ))"
+  echo "--------"
+  echo "-- test details"
+  echo "--------"
+
+  echo "### Test cases reproved list: "
+  for test in $( echo $TESTS_REPROVED_LIST | tr ':' '\n' )
+  do
+    echo "    x $test"
+  done
+
+  echo "### Test cases approved list: "
+  for test in $( echo $TESTS_PASSED_LIST | tr ':' '\n' )
+  do
+    echo "    - $test"
+  done
+
 }
 
 ###########
@@ -28,8 +65,7 @@ function 0001_up_n_running {
   echo "--- $TEST_CODE - $TEST_DESCRIPTION "
 
   VERB="GET"
-  API=""
-  echo "--- $TEST_CODE - $TEST_DESCRIPTION "
+  API=""  
   [[ $( curl  -X $VERB -s "$PROTOCOL://$HOST:$PORT/$API" | grep "<!-- HTML for static distribution bundle build -->" -c ) == "0" ]] && rejected $TEST_CODE 
  
   checked $TEST_CODE
@@ -85,4 +121,7 @@ function 2001_joblist_return_any_json {
 2001_joblist_return_any_json
 
 
-
+###########
+### Print test resume  
+###########
+echo_resume
